c5fff5c6931bd2283ccdaa480161e355e8be775f
aklavya20 <aklavya36@gmail.com>
2025-07-01 12:22:31 +0530

    initial commit

--- src/Body.jsx
+++ src/Body.jsx
@@ -5,19 +5,12 @@ const Body = ({ datas }) => {
       {datas &&
         datas.map((filteredData, index) => (
           <div key={index}>
-            {/* <div>{filteredData.id}</div> */}
+           
             <div>{filteredData.name}</div>
-            {/* <div>{filteredData.email}</div> */}
+            
           </div>
         ))}
-      {/* {datas.filteredData.map((ele) => (
-        <div key={ele.id}>
-          <div>{ele.id}</div>
-          <div>{ele.name}</div>
-          <div>{ele.email}</div>
-          <br />
-        </div>
-      ))} */}
+      
     </div>
   );
 };
